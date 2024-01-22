05e187f7ba6e8d34593ddedf008bdb65b2076f99
azdrogov <azdrogov@naumen.ru>
2024-01-22 22:04:07 +0500

    Добавил все команды для работы с ocpasswd

--- build.sbt
+++ build.sbt
@@ -7,7 +7,6 @@ version := "0.1"
 scalaVersion := "2.13.12"
 
 libraryDependencies ++= Seq(
-    "org.http4s" %% "http4s-blaze-server" % "0.23.16",
     "org.http4s" %% "http4s-ember-server" % "0.23.16",
     "dev.zio" %% "zio" % "2.0.21",
     "com.softwaremill.sttp.tapir" %% "tapir-core" % "1.9.6",
--- src/main/scala/KorovavpoleServer.scala
+++ src/main/scala/KorovavpoleServer.scala
@@ -1,3 +1,4 @@
+import cats.implicits._
 import com.comcast.ip4s._
 import org.http4s.implicits._
 import org.http4s.ember.server.EmberServerBuilder
@@ -8,6 +9,7 @@ import zio.{RIO, ZIO}
 
 import sys.process._
 import org.http4s.HttpRoutes
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import zio.interop.catz.asyncInstance
 
 object KorovavpoleServer {
@@ -16,21 +18,84 @@ object KorovavpoleServer {
 
     type AppRIO[A] = RIO[AppEnv, A]
 
-    private val routes: HttpRoutes[AppRIO] = {
-        val helloWorldEndpoint: ZServerEndpoint[Any, Any] =
-            endpoint.get
-                .in("hello")
-                .in(query[String]("login"))
-                .in(query[String]("password"))
-                .errorOut(stringBody)
-                .out(stringBody)
-                .zServerLogic { case (login, password) =>
-                    (s"echo $password" #| s"ocpasswd -c /home/azdrogov/ocserv.passwd $login").! match {
-                        case 1 => ZIO.succeed("Success. Пользователь создан")
-                        case 0 => ZIO.fail("Fail. Ошибка при создании пользователя")
-                    }
+    private val createUserEndpoint =
+        endpoint.post
+            .in("user" / "create")
+            .in(query[String]("login").example("Korova"))
+            .in(query[String]("password").example("qwerty"))
+            .out(stringBody.example("Success. Пользователь создан"))
+            .errorOut(stringBody.example("Fail. Ошибка при создании пользователя"))
+            .description("Создаёт пользователя по логину и паролю")
+
+    private val deleteUserEndpoint =
+        endpoint.delete
+            .in("user" / "delete")
+            .in(query[String]("login").example("Korova"))
+            .out(stringBody.example("Success. Пользователь удалён"))
+            .errorOut(stringBody.example("Fail. Ошибка при удаление пользователя"))
+            .description("Удаляет пользователя по логину")
+
+    private val lockUserEndpoint =
+        endpoint.post
+            .in("user" / "lock")
+            .in(query[String]("login").example("Korova"))
+            .out(stringBody.example("Success. Пользователь заблокирован"))
+            .errorOut(stringBody.example("Fail. Ошибка при блокировке пользователя"))
+            .description("Блокирует пользователя по логину")
+
+    private val unlockUserEndpoint =
+        endpoint.post
+            .in("user" / "unlock")
+            .in(query[String]("login").example("Korova"))
+            .out(stringBody.example("Success. Пользователь разблокирован"))
+            .errorOut(stringBody.example("Fail. Ошибка при разблокировке пользователя"))
+            .description("Разблокирует пользователя по логину")
+
+    private val createUserEndpointLogic: ZServerEndpoint[Any, Any] =
+        createUserEndpoint
+            .zServerLogic { case (login, password) =>
+                (s"echo $password" #| s"ocpasswd -c /home/azdrogov/ocserv.passwd $login").! match {
+                    case 0 => ZIO.succeed("Success. Пользователь создан")
+                    case 1 => ZIO.fail("Fail. Ошибка при создании пользователя")
                 }
-        ZHttp4sServerInterpreter().from(helloWorldEndpoint).toRoutes
+            }
+
+    private val deleteUserEndpointLogic: ZServerEndpoint[Any, Any] =
+        deleteUserEndpoint
+            .zServerLogic { login =>
+                s"ocpasswd -c /home/azdrogov/ocserv.passwd -d $login".! match {
+                    case 0 => ZIO.succeed("Success. Пользователь удалён")
+                    case 1 => ZIO.fail("Fail. Ошибка при удаление пользователя")
+                }
+            }
+
+    private val lockUserEndpointLogic: ZServerEndpoint[Any, Any] =
+        lockUserEndpoint
+            .zServerLogic { login =>
+                s"ocpasswd -c /home/azdrogov/ocserv.passwd -l $login".! match {
+                    case 0 => ZIO.succeed("Success. Пользователь заблокирован")
+                    case 1 => ZIO.fail("Fail. Ошибка при блокировке пользователя")
+                }
+            }
+
+    private val unlockUserEndpointLogic: ZServerEndpoint[Any, Any] =
+        unlockUserEndpoint
+            .zServerLogic { login =>
+                s"ocpasswd -c /home/azdrogov/ocserv.passwd -u $login".! match {
+                    case 0 => ZIO.succeed("Success. Пользователь разблокирован")
+                    case 1 => ZIO.fail("Fail. Ошибка при разблокировке пользователя")
+                }
+            }
+
+    private val routes: HttpRoutes[AppRIO] = {
+        ZHttp4sServerInterpreter().from(List(createUserEndpointLogic, deleteUserEndpointLogic, lockUserEndpointLogic, unlockUserEndpointLogic)).toRoutes
+    }
+
+    private val routesSwagger: HttpRoutes[AppRIO] = {
+        ZHttp4sServerInterpreter()
+            .from(SwaggerInterpreter()
+                .fromEndpoints[AppRIO](List(createUserEndpoint, deleteUserEndpoint, lockUserEndpoint, unlockUserEndpoint), "Korova Admin", "1.0")
+            ).toRoutes
     }
 
     def run: AppRIO[Nothing] = {
@@ -39,7 +104,7 @@ object KorovavpoleServer {
                     .default[AppRIO]
                     .withHost(ipv4"127.0.0.1")
                     .withPort(port"8080")
-                    .withHttpApp(Router("/" -> routes).orNotFound)
+                    .withHttpApp(Router("/" -> (routes <+> routesSwagger)).orNotFound)
                     .build
         } yield ()
     }.useForever
