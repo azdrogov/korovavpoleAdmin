0fac208778ff375dc00a6adc78a171590f045cc0
azdrogov <azdrogov@naumen.ru>
2024-01-22 16:54:18 +0500

    Fixes. It works

--- src/main/scala/KorovavpoleServer.scala
+++ src/main/scala/KorovavpoleServer.scala
@@ -1,86 +1,46 @@
-import cats.syntax.all._
-import io.circe.generic.auto._
 import com.comcast.ip4s._
-import org.http4s.Method.GET
-import org.http4s.Uri.Path.Root
-import org.http4s._
 import org.http4s.implicits._
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
-import sttp.tapir.PublicEndpoint
 import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
-import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import sttp.tapir.ztapir._
-import zio.interop.catz._
-import zio.{ExitCode, RIO, Task, UIO, URIO, ZIO, ZIOAppDefault}
-import org.http4s.server.middleware.Logger
+import zio.{RIO, ZIO}
 
-import com.comcast.ip4s.{Host, Port}
+import sys.process._
 import org.http4s.HttpRoutes
-import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.Router
-import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
-import sttp.tapir.swagger.bundle.SwaggerInterpreter
-import zio._
 import zio.interop.catz.asyncInstance
 
 object KorovavpoleServer {
-
-/*    val helloWorldEndpoint: PublicEndpoint[(String, String), String, String, Any] =
-        endpoint.get
-            .in("hello")
-            .in(query[String]("login"))
-            .in(query[String]("password"))
-            .errorOut(stringBody)
-            .out(stringBody)
-
-    val helloWorldRoutes: HttpRoutes[Task] = ZHttp4sServerInterpreter().from(
-            helloWorldEndpoint.zServerLogic { case (login, password) =>
-            ZIO.succeed(s"$login, $password, МОЛОДЕЦ")
-        }).toRoutes*/
-/*
-    val healthCheck = endpoint.get.in("health").out(stringBody)
-    val healthRoute: HttpRoutes[Task] = ZHttp4sServerInterpreter()
-        .from(healthCheck.zServerLogic(_ => ZIO.succeed("Привет"))).toRoutes
-
-
-    def run = {
-        for {
-            _ <- EmberServerBuilder
-                    .default[Task]
-                    .withHost(ipv4"127.0.0.1")
-                    .withPort(port"9090")
-                    .withHttpApp(Router("/" -> healthRoute).orNotFound)
-                    .build
-        } yield ()
-    }.useForever*/
 
     type AppEnv = Any
 
     type AppRIO[A] = RIO[AppEnv, A]
 
     private val routes: HttpRoutes[AppRIO] = {
-        val serverEndpoints: ZServerEndpoint[Any, Any] =
-            endpoint.get.in("health").out(stringBody)
-                .zServerLogic {
-                    _ => {
-                        ZIO.succeed(println("AAA"))
-                        ZIO.succeed("Привет")
+        val helloWorldEndpoint: ZServerEndpoint[Any, Any] =
+            endpoint.get
+                .in("hello")
+                .in(query[String]("login"))
+                .in(query[String]("password"))
+                .errorOut(stringBody)
+                .out(stringBody)
+                .zServerLogic { case (login, password) =>
+                    (s"echo $password" #| s"ocpasswd -c /home/azdrogov/ocserv.passwd $login").! match {
+                        case 1 => ZIO.succeed("Success. Пользователь создан")
+                        case 0 => ZIO.fail("Fail. Ошибка при создании пользователя")
                     }
                 }
-
-        ZHttp4sServerInterpreter().from(List(serverEndpoints) ).toRoutes
+        ZHttp4sServerInterpreter().from(helloWorldEndpoint).toRoutes
     }
 
-    def run() =
+    def run: AppRIO[Nothing] = {
         for {
-            _ <-
-                EmberServerBuilder
+            _ <- EmberServerBuilder
                     .default[AppRIO]
                     .withHost(ipv4"127.0.0.1")
                     .withPort(port"8080")
                     .withHttpApp(Router("/" -> routes).orNotFound)
                     .build
-                    .useForever
         } yield ()
+    }.useForever
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,6 @@
 import zio.{ExitCode, URIO, ZIOAppDefault}
-import org.http4s.implicits._
-import cats.syntax.all._
 
 object Main extends ZIOAppDefault {
     override def run: URIO[Any, ExitCode] =
-        KorovavpoleServer.run().provide().exitCode
+        KorovavpoleServer.run.provide().exitCode
 }
